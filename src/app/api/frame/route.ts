63c27cff88ad2acb4a80fbc3f7a4347aa680964b
earth2travis <travis.mccutcheon@gmail.com>
2024-02-02 18:44:00 -0600

    Diarrhea

--- src/app/api/frame/route.ts
+++ src/app/api/frame/route.ts
@@ -1,19 +1,19 @@
-import { NextRequest, NextResponse } from "next/server";
-import { Message, getSSLHubRpcClient } from "@farcaster/hub-nodejs";
-import { FrameRequest } from "../../types/farcasterTypes";
+import { NextRequest, NextResponse } from 'next/server';
+import { Message, getSSLHubRpcClient } from '@farcaster/hub-nodejs';
+import { FrameRequest } from '../../types/farcasterTypes';
 
-const POST_URL = "https://frame-airdrop.vercel.app/api/frame";
+const POST_URL = 'https://frames-yeet.vercel.app/api/frame';
 
 export async function POST(req: NextRequest, res: NextResponse) {
-  console.log("POST received at /api/frame");
+  console.log('POST received at /api/frame');
 
-  const HUB_URL = process.env["HUB_URL"] || "nemes.farcaster.xyz:2283";
+  const HUB_URL = process.env['HUB_URL'] || 'nemes.farcaster.xyz:2283';
   const client = getSSLHubRpcClient(HUB_URL);
   let validatedMessage: Message | undefined = undefined;
   try {
     const body: FrameRequest = await req.json();
     const frameMessage = Message.decode(
-      Buffer.from(body?.trustedData?.messageBytes || "", "hex")
+      Buffer.from(body?.trustedData?.messageBytes || '', 'hex')
     );
     const result = await client.validateMessage(frameMessage);
     if (result.isOk() && result.value.valid) {
@@ -22,8 +22,8 @@ export async function POST(req: NextRequest, res: NextResponse) {
 
     // Also validate the frame url matches the expected url
     let urlBuffer = validatedMessage?.data?.frameActionBody?.url || [];
-    const urlString = Buffer.from(urlBuffer).toString("utf-8");
-    if (!urlString.startsWith(process.env["HOST"] || "")) {
+    const urlString = Buffer.from(urlBuffer).toString('utf-8');
+    if (!urlString.startsWith(process.env['HOST'] || '')) {
       throw new Error(`Invalid frame url: ${urlBuffer}`);
     }
   } catch (e) {
@@ -35,8 +35,7 @@ export async function POST(req: NextRequest, res: NextResponse) {
 
   console.log(`Pressed button ${buttonId} with fid ${fid}`);
 
-  const NEW_IMAGE_URLimage =
-    "https://assets.blocklive.io/events/2bfb5de2-cd38-49d5-bb3b-1834dfc66380/58662d9d-303b-4c72-b3cd-92207ca8b91b_dall_e_2024_01_28_23_08_18___design_a_banner_image__size_936px_x_468px__that_looks_like_an_entrance_ticket_to_an_event_for_the__frames__feature_on_a_social_media_platform_resembli.png";
+  const NEW_IMAGE_URLimage = 'https://frames-yeet.vercel.app/diarrhea.png';
 
   let html =
     `<!DOCTYPE html><html><head>` +
@@ -47,7 +46,7 @@ export async function POST(req: NextRequest, res: NextResponse) {
     `<meta property="fc:frame:post_url" content="${POST_URL}" />` +
     `</head></html>`;
 
-  return new Response(html, { headers: { "Content-Type": "text/html" } });
+  return new Response(html, { headers: { 'Content-Type': 'text/html' } });
 }
 
-export const dynamic = "force-dynamic";
+export const dynamic = 'force-dynamic';
